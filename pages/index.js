d435f2f7d35c9f562cb21d43e9fdc787d35a3258
Marlon Huber-Smith <marlon.huber-smith@akqa.com>
2018-12-03 00:31:11 +0100

    Add crate styling. Fix 3d rendering of records in crates. Add simple  hover state to records

--- pages/index.js
+++ pages/index.js
@@ -8,42 +8,108 @@ const setBackground = item => assoc("background", randomColor())(item);
 const itemLens = lensProp("items");
 const transformDataToCrates = pipe(over(itemLens, map(setBackground)));
 
-const crates = repeat(transformDataToCrates(mockData), 5);
+const data = repeat(transformDataToCrates(mockData), 5);
 
-const Record = (record, index) => (
-  <li key={record.id}>
+const CRATE_RECT = {
+  width: 200,
+  height: 100,
+  depth: 100
+};
+
+const getWindow = () => (typeof window === "undefined" ? {} : window);
+
+const RECORD_SIZE = 180;
+
+const Record = (record, index) => {
+  const [isPeeking, peek] = useState(false);
+  const transform = `translate3d(
+    ${(CRATE_RECT.width - RECORD_SIZE) / 2}px,
+    ${-RECORD_SIZE / 2}px,
+    -${(index * 3 + 1) * 2}px
+  )`;
+  return (
+    <li key={record.id}>
+      <style jsx>
+        {`
+          li {
+            width: ${RECORD_SIZE}px;
+            height: ${RECORD_SIZE}px;
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            transform: ${transform};
+            background: ${record.background};
+            list-style: none;
+          }
+
+          li:hover {
+            transform: ${transform} translateY(-10px);
+          }
+        `}
+      </style>
+    </li>
+  );
+};
+
+const Crate = ({ items, mousePosition, index }) => {
+  const yRotation = 360 - 0 * 20;
+  return (
+    <ul>
+      <style jsx>
+        {`
+          ul {
+            width: ${CRATE_RECT.width}px;
+            height: ${CRATE_RECT.height}px;
+            position: absolute;
+            top: 50%;
+            transform-style: preserve-3d;
+            left: calc(${index * 20}vw);
+            background: grey;
+            transform: rotateX(${-25 + mousePosition.y * 10}deg)
+              rotateY(${yRotation + mousePosition.x * 10}deg);
+          }
+
+          ul:before,
+          ul:after {
+            display: block;
+            content: "";
+            width: ${CRATE_RECT.depth}px;
+            height: ${CRATE_RECT.height}px;
+            position: absolute;
+            background: darkgrey;
+          }
+
+          ul:before {
+            transform: translate3d(${-CRATE_RECT.depth / 2}px, 0, 0)
+              rotateY(-90deg) translate3d(${-CRATE_RECT.depth / 2}px, 0, 0);
+          }
+
+          ul:after {
+            transform: translate3d(${CRATE_RECT.depth * 1.5}px, 0, 0)
+              rotateY(-90deg) translate3d(${-CRATE_RECT.depth / 2}px, 0, 0);
+          }
+        `}
+      </style>
+      {addIndex(map)(Record, items)}
+    </ul>
+  );
+};
+
+const Crates = ({ crates, mousePosition }) => (
+  <div>
     <style jsx>
       {`
-        li {
-          width: 200px;
-          height: 200px;
-          position: absolute;
-          top: ${index}px;
-          left: ${index}px;
-          background: ${record.background};
-          list-style: none;
+        div {
         }
       `}
     </style>
-  </li>
-);
-
-const Crate = ({ items, mousePosition, index }) => (
-  <ul>
-    <style jsx>
-      {`
-        ul {
-          width: 200px;
-          height: 200px;
-          position: absolute;
-          top: 50%;
-          left: calc(${index * 20}vw);
-          background: grey;
-        }
-      `}
-    </style>
-    {addIndex(map)(Record, items)}
-  </ul>
+    {addIndex(map)(
+      (crate, index) => (
+        <Crate {...crate} mousePosition={mousePosition} index={index} />
+      ),
+      crates
+    )}
+  </div>
 );
 
 const mouseMove = hook => event => {
@@ -52,6 +118,11 @@ const mouseMove = hook => event => {
     y: event.clientY
   });
 };
+
+const asPercentage = ({ x, y }) => ({
+  x: x / getWindow().innerWidth,
+  y: y / getWindow().innerHeight
+});
 
 export default () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -64,18 +135,14 @@ export default () => {
             width: 100vw;
             height: 100vh;
             display: block;
-            background: ${randomColor()};
+            background: black;
             position: relative;
+            perspective: 800px;
           }
         `}
       </style>
 
-      {addIndex(map)(
-        (crate, index) => (
-          <Crate {...crate} mousePosition={mousePosition} index={index} />
-        ),
-        crates
-      )}
+      <Crates crates={data} mousePosition={asPercentage(mousePosition)} />
     </main>
   );
 };
